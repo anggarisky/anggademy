1c24001d8983c9ef79f31059c56e661c057d60df
Angga <anggariskysetiawan@gmail.com>
2022-09-23 07:57:13 +0700

    new seo

--- resources/views/front/course.blade.php
+++ resources/views/front/course.blade.php
@@ -3,9 +3,35 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Free How-To Design & Code Tutorials Online by Anggademy</title>
+    <meta name="description" content="Anggademy provides updated daily tutorials, learn over 10,000 design and code how-to tutorials. Improve your software development skills, ui/ux design, web design, and more.">
+    <meta name="keywords" content="Flutter, Android Studio, Figma, SwiftUI, HTML CSS, Bootstrap, Tailwind CSS, Laravel, React Native, Webflow, JavaScript, Adobe XD, Web Design, Mobile App Development">
+    <meta name="author" content="Anggademy" />
+
+    <meta image="{{ asset('images/logo-anggademy.png') }}">
+    <meta property="og:type" content="article">
+    <meta property="og:image" content="{{ asset('images/logo-anggademy.png') }}">
+    <meta content="index, follow" name="robots" />
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Free How-To Design & Code Tutorials Online by Anggademy">
+    <meta property="og:site_name" content="Anggademy">
+    <meta property="og:url" content="https://anggademy.com/course">
+    <meta property="og:description" content="Anggademy provides updated daily tutorials, learn over 10,000 design and code how-to tutorials. Improve your software development skills, ui/ux design, web design, and more.">
+
+    <link rel="icon" href="{{ asset('images/logo-anggademy.png') }}"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  </head>
+    
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-61SC4TGKHQ"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-61SC4TGKHQ');
+    </script>
+
+</head>
   <body>
     <div class="container my-5 py-5">
         <div class="row mb-5">
