427a5f0c879c0660f1a5972e66bd5d6f15fcc5f6
Angga <anggariskysetiawan@gmail.com>
2022-09-26 10:42:50 +0700

    paging

--- resources/views/front/course.blade.php
+++ resources/views/front/course.blade.php
@@ -20,6 +20,7 @@
 
     <link rel="icon" href="{{ asset('images/logo-anggademy.png') }}"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     
     <!-- Google tag (gtag.js) -->
